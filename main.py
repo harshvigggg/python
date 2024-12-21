06462dd6a713a6c2d85675cd1818650e20a014a8
Harsh vig <harshvigg@gmail.com>
2024-12-21 19:55:16 +0530

    created the first function to collect the bet amount from the user only if the amount is valid, i.e number, greate than 0

---
+++ main.py
@@ -0,0 +1,15 @@
+def deposit():
+    while True:
+        amount = input("enter the amount you want to deposit:")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break;
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number")
+    return amount
+
+
+
