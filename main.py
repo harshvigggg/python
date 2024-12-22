21cf0c1e7df3521c7d25b0a265528f392746682d
Harsh vig <harshvigg@gmail.com>
2024-12-22 17:24:35 +0530

    ended making the slot machine what left is the checking if you wont i mean if you got 3 same symbols in a row

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+        "A":2,
+        "B":4,
+        "C":6,
+        "D":8
+        }
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol,symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbol = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbol)
+            current_symbol.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i,column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row],end=" | ")
+            else:
+                print(column[row],end="")
+
+        print()
+
+
 def deposit():
     while True:
         amount = input("enter the amount you want to deposit:")
@@ -49,5 +91,9 @@ def main():
         print("bet amount exceeded the balance, try again!")
         betamount = get_bet()
     print(f"you are betting rupees {betamount} on {lines} lines, total bet is equal to: rupees {lines*betamount}")
+    
+    slots = get_slot_machine_spin(ROWS,COLS,symbol_count)
+    print_slot_machine(slots)
+
 
 main()
